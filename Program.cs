380c7f19c6fd0b8b9649407f203eb6d283d9f232
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2022-09-04 15:11:46 +0500

    First task

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Seminar4HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите цикл , который принимает на вход 2 числа (А и В) и возводит число А в натуральную степень В
+
+            void Pow (int A, int B){
+                Console.WriteLine ($"Answer is {Math.Pow(A,B)}");
+            }
+
+            Console.WriteLine ("Enter number A: ");
+            int numA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Enter number B: ");
+            int numB = Convert.ToInt32(Console.ReadLine());
+            Pow(numA,numB);
+
+
+            //Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+            //Напишите программу, которая задает массив из произвольного элемента и выводит их на экран
+        }
+    }
+}
