b2eca0dcf77f335f90ce5f7229dedc9319bcede5
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2022-09-08 11:35:03 +0500

    All done

--- Program.cs
+++ Program.cs
@@ -8,37 +8,39 @@ namespace Seminar4HomeWork
         {
             //Напишите цикл , который принимает на вход 2 числа (А и В) и возводит число А в натуральную степень В
 
-            int Num (int A, int B){
-                int res = 1;
-                for(int count = 0; count < B; count++){
-                    res*= A;
-                }
-            return res;
-            }
+            // int Num (int A, int B){
+            //     int res = 1;
+            //     for(int count = 0; count < B; count++){
+            //         res*= A;
+            //     }
+            // return res;
+            // }
 
-            Console.WriteLine ("Enter the number A: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine ("Enter number B: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine ("Enter the number A: ");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine ("Enter number B: ");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
 
-            int result = Num (num1, num2);
-            Console.WriteLine("Number is - " + result);
+            // int result = Num (num1, num2);
+            // Console.WriteLine("Number is - " + result);
                          
 
             //Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
             // что-то с формулой не так
-            // int Sum (int num){
-            //     int result=0;
-            //         for (int curr = num; curr!=0; curr -= curr%10){
-            //             result += curr;
-            //         }
-            //     return result;                
-            // }
+            int Sum (int num){
+                int result=0;
+                    for (int curr = num; curr>0; curr /=10){
+                        result += curr%10;
+                    }
+                return result;                
+            }
 
-            // Console.WriteLine("Enter the number: ");
-            // int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
             
-            // Sum (num);
+            //int number =  ();
+            Console.WriteLine($"Result is - {Sum (num)}");
+            
 
             //Напишите программу, которая задает массив из произвольного элемента и выводит их на экран
 
